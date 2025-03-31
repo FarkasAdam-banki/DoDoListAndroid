c1af537fc07a1add65c6b6303beeba57e50aa120
FarkasAdam-banki <77265777+FarkasAdam-banki@users.noreply.github.com>
2025-03-31 13:02:02 +0200

    Feladat határidejének kiiratása

--- app/src/main/java/com/example/dodolist/ui/theme/TaskSettingsActivity.kt
+++ app/src/main/java/com/example/dodolist/ui/theme/TaskSettingsActivity.kt
@@ -4,10 +4,12 @@ import Model.TaskDetails
 import TaskService
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dodolist.R
 import com.example.dodolist.network.RetrofitClient
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import retrofit2.Call
 import retrofit2.Callback
@@ -16,12 +18,18 @@ import retrofit2.Response
 class TaskSettingsActivity : AppCompatActivity() {
     private lateinit var textInputLayout: TextInputLayout
     private lateinit var taskService: TaskService
+    private lateinit var titleTextView:TextView
+    private lateinit var dateInputLayout: TextInputLayout
+    private lateinit var timeInputLayout: TextInputLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasksettings)
 
         textInputLayout = findViewById(R.id.textInputLayout)
+        titleTextView = findViewById(R.id.titleTextView)
+        dateInputLayout = findViewById(R.id.dateInputLayout)
+        timeInputLayout = findViewById(R.id.timeInputLayout)
 
         val taskId = intent.getIntExtra("TASK_ID", -1)
         if (taskId != -1) {
@@ -39,6 +47,25 @@ class TaskSettingsActivity : AppCompatActivity() {
                     val task = response.body()
                     if (task != null) {
                         textInputLayout.editText?.setText(task.feladat_nev)
+                        titleTextView.setText("${task.feladat_nev} feladat beállitásai")
+
+                        val deadline = task.feladat_hatarido
+                        val dateTimeParts = deadline.split(" ")
+                        if (dateTimeParts.size == 2) {
+                            val date = dateTimeParts[0]
+                            val time = dateTimeParts[1]
+
+                            val dateParts = date.split("-")
+                            if (dateParts.size == 3) {
+                                val year = dateParts[0]
+                                val month = dateParts[1]
+                                val day = dateParts[2]
+                                val dateText = "$year-$month-$day"
+                                dateInputLayout.editText?.setText(dateText)
+                            }
+
+                            timeInputLayout.editText?.setText(time)
+                        }
                     } else {
                         Toast.makeText(this@TaskSettingsActivity, "A feladat adatai üresek!", Toast.LENGTH_SHORT).show()
                     }
